41f239016d7bbbc2f883299843a2b8de86b1cdbf
Mark29032011 <igromalcik@gmail.com>
2025-10-07 19:46:47 +0200

    step-2

--- src/main.js
+++ src/main.js
@@ -3,9 +3,64 @@ const input = document.getElementById('search');
 const button = document.getElementById('searchBtn');
 const results = document.getElementById('results');
 
+const TOP_MOVIES = [
+  'tt15398776',
+  'tt6710474',
+  'tt1877830',
+  'tt13320622',
+  'tt10366206',
+  'tt1630029',
+  'tt14209916',
+  'tt15671028',
+  'tt13238346',
+  'tt11358390',
+  'tt10298810',
+  'tt10151854',
+];
+
+function displayMoviesByIds(imdbIds) {
+  if (imdbIds.length === 0) {
+    results.innerHTML = '<p>No movies found</p>';
+    return;
+  }
+
+  const promises = imdbIds.map(id =>
+    fetch(`https://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`).then(response =>
+      response.json()
+    )
+  );
+
+  Promise.all(promises)
+    .then(fullMovies => {
+      results.innerHTML = fullMovies
+        .map(
+          movie => `
+            <div class="movie-card">
+              <img src="${
+                movie.Poster === 'N/A'
+                  ? 'https://via.placeholder.com/200x300?text=No+Poster'
+                  : movie.Poster
+              }" />
+              <div class="movie-info">
+                <h3>${movie.Title} (${movie.Year})</h3>
+                <p>Director: ${movie.Director}</p>
+                <p>Actors: ${movie.Actors}</p>
+                <p>IMDb: ${movie.imdbRating || 'N/A'}</p>
+                <p>${movie.Plot || 'No description'}</p>
+              </div>
+            </div>
+          `
+        )
+        .join('');
+    })
+    .catch(() => {
+      results.innerHTML = '<p>Error loading movies</p>';
+    });
+}
+
 function searchMovies(query) {
   if (query === '') {
-    results.innerHTML = '';
+    displayMoviesByIds(TOP_MOVIES);
     return;
   }
 
@@ -15,36 +70,8 @@ function searchMovies(query) {
     .then(response => response.json())
     .then(data => {
       if (data.Response === 'True') {
-        const movies = data.Search.slice(0, 12);
-
-        const detailPromises = movies.map(movie =>
-          fetch(
-            `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${API_KEY}`
-          ).then(res => res.json())
-        );
-
-        Promise.all(detailPromises).then(fullMovies => {
-          results.innerHTML = fullMovies
-            .map(
-              movie => `
-              <div class="movie-card">
-                <img src="${
-                  movie.Poster === 'N/A'
-                    ? 'https://via.placeholder.com/300?text=No+Poster'
-                    : movie.Poster
-                }"/>
-                <div class="info">
-                  <h3>${movie.Title} (${movie.Year})</h3>
-                  <p>Director:${movie.Director}</p>
-                  <p>Actors:${movie.Actors}</p>
-                  <p>IMDb: ${movie.imdbRating || 'N/A'}</p>
-                  <p class="plot">${movie.Plot}</p>
-                </div>
-              </div>
-            `
-            )
-            .join('');
-        });
+        const ids = data.Search.slice(0, 12).map(m => m.imdbID);
+        displayMoviesByIds(ids);
       } else {
         results.innerHTML = '<p>No movies found</p>';
       }
@@ -53,6 +80,8 @@ function searchMovies(query) {
       results.innerHTML = '<p>Error loading movies</p>';
     });
 }
+
+displayMoviesByIds(TOP_MOVIES);
 
 button.addEventListener('click', () => searchMovies(input.value));
 input.addEventListener('keypress', e => {
